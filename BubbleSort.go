6035d83feb05f9bf89ed1633328bbf94b76617de
keith <keith@skmcdowelljr.com>
2023-02-19 13:15:09 -0500

    Initialize files

---
+++ BubbleSort.go
@@ -0,0 +1,18 @@
+package main
+
+// BubbleSort_Course2_Week1
+
+func main() {
+}
+
+func userInput() {
+
+}
+
+func BubbleSort() {
+
+}
+
+func Swap() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Course2_week1
+
+go 1.20
