6b8daefdf7589de34ecce5fbdc2b8a9a6ecd34d6
keith <keith@skmcdowelljr.com>
2023-02-19 13:15:09 -0500

    Initialize files

--- BubbleSort.go
+++ BubbleSort.go
@@ -1,13 +1,26 @@
 package main
 
+import "fmt"
+
 // BubbleSort_Course2_Week1
+var numberSlice []int
 
 func main() {
+	fmt.Println("Enter 10 + or - numbers to sort ")
+	fmt.Scan(&numberSlice)
 }
 
+/*
 func userInput() {
 
-}
+	fmt.Println("Enter 10 + or - numbers to sort ")
+	fmt.Scanln(number_slice)
+	/*
+		fmt.Println("Here is your unsorted list of Integers below. ")
+		number_slice := append(number_slice)
+		fmt.Println(number_slice)
+
+*/
 
 func BubbleSort() {
 
